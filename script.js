9c709068ec17db3b00de0b495f463793d99bea49
Oliwier <olikucz@vp.pl>
2021-11-06 16:31:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,608 @@
+const arrayOfSongs = [
+  {
+    songCover: "./img/total.jpg",
+    title: "True Faith",
+    artist: "New Order",
+    song: "./audio/True Faith.mp3",
+    duration: "4.19",
+    lyrics: `
+    <p>
+      <span class="indicators">[Verse 1]</span>
+      I feel so extraordinary
+      Something's got a hold on me
+      I get this feeling I'm in motion
+      A certain sense of liberty
+      I don't care 'cause I'm not there
+      And I don't care if I'm here tomorrow
+      Again and again I've taken too much
+      Of the things that cost you too much
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      I used to think that the day would never come
+      I'd see the light in the shade of the morning Sun
+      My morning sun is the drug that brings me near
+      To the childhood I lost, replaced by fear
+      I used to think that the day would never come
+      That my life would depend on the morning Sun
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 2]</span>
+      When I was a very small boy
+      Very small boys talked to me
+      Now that we've grown up together
+      They're afraid of what they see
+      That's the price that we all pay
+      And the value of destiny comes to nothing
+      I can't tell you where we're going
+      I guess there was just no way of knowing
+      [Chorus]
+      I used to think that the day would never come
+      I'd see the light in the shade of the morning sun
+      My morning sun is the drug that brings me near
+      To the childhood I lost, replaced by fear
+      I used to think that the day would never come
+      That my life would depend on the morning sun...
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 3]</span>
+      I feel so extraordinary
+      Something's got a hold on me
+      I get this feeling I'm in motion
+      A certain sense of liberty
+      The chances are we've gone too far
+      You took my time and you took my money
+      Now I fear you've left me standing
+      In a world that's so demanding
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      I used to think that the day would never come
+      I'd see the light in the shade of the morning sun
+      My morning sun is the drug that brings me near
+      To the childhood I lost, replaced by fear
+      I used to think that the day would never come
+      That my life would depend on the morning sun...
+      I used to think that the day would never come
+      I'd see the light in the shade of the morning sun
+      My morning sun is the drug that brings me near
+      To the childhood I lost, replaced by fear
+      I used to think that the day would never come
+      That my life would depend on the morning sun...
+      On the morning sun...
+      On the morning sun...
+      On the morning sun...
+      On the morning sun...
+      On the morning sun...
+      On the morning sun...
+    </p>
+    `,
+    details: `<p>Title: True Faith</p>
+    <p>Artist: New Order</p>
+    <p>Released: 1987</p>
+    <p>Album: Substance</p>
+    <p>Songwriter(s):	
+    Gillian Gilbert ,Stephen Hague, Peter Hook,Stephen Morris, Bernard Sumner</p> <p>Producer(s):	
+    New Order, Stephen Hague</p> `,
+  },
+  {
+    songCover: "./img/220px-Whitney_Houston_–_The_Greatest_Love_of_All.jpg",
+    title: "Greatest Love Of Love",
+    artist: "Whitney Houston",
+    song: "./audio/Greatest Love Of All.mp3",
+    duration: "4.51",
+    lyrics: `
+    <p>
+      <span class="indicators">[Verse 1]</span>
+      I believe the children are our future
+      Teach them well and let them lead the way
+      Show them all the beauty they possess inside
+      Give them a sense of pride to make it easier
+      Let the children's laughter remind us how we used to be
+      Everybody's searching for a hero
+      People need someone to look up to
+      I never found anyone who fulfilled my needs
+      A lonely place to be
+      And so I learned to depend on me
+    </p>
+
+    <p>
+      <span class="indicators">[Pre-Chorus]</span>
+      I decided long ago
+      Never to walk in anyone's shadows
+      If I fail, if I succeed
+      At least I'll live as I believe
+      No matter what they take from me
+      They can't take away my dignity
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      Because the greatest love of all
+      Is happening to me
+      I found the greatest love of all
+      Inside of me
+      The greatest love of all
+      Is easy to achieve
+      Learning to love yourself
+      It is the greatest love of all
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 2]</span>
+      I believe the children are our future
+      Teach them well and let them lead the way
+      Show them all the beauty they possess inside
+      Give them a sense of pride to make it easier
+      Let the children's laughter remind us how we used to be
+    </p>
+
+    <p>
+      <span class="indicators">[Pre-Chorus]</span>
+      I decided long ago, never to walk in anyone's shadows
+      If I fail, if I succeed
+      At least I'll live as I believe
+      No matter what they take from me
+      They can't take away my dignity
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      Because the greatest love of all
+      Is happening to me
+      I found the greatest love of all
+      Inside of me
+      The greatest love of all
+      Is easy to achieve
+      Learning to love yourself
+      It is the greatest love of all
+    </p>
+
+    <p>
+      <span class="indicators">[Outro]</span>
+      And if, by chance, that special place
+      That you've been dreaming of
+      Leads you to a lonely place
+      Find your strength in love
+    </p>
+    `,
+    details: "",
+  },
+  {
+    songCover: "./img/R-6487215-1420390189-2540.jpeg.jpg",
+    title: "Hey Joe",
+    artist: "Jimi Hendrix",
+    song: "./audio/Hey Joe.mp3",
+    duration: "3.47",
+    lyrics: `
+    <p><span class="indicators">[Instrumental Intro]</span></p>
+
+    <p>
+      <span class="indicators">[Verse 1]</span>
+      Hey Joe, where you going with that gun in your hand?
+      Hey Joe, I said, where you going with that gun in your hand?
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 2]</span>
+      I'm going down to shoot my old lady
+      You know, I caught her messing around with another man
+      I'm going down to shoot my old lady
+      You know, I caught her messing around with another man
+      And that ain't too cool
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 3]</span>
+      Hey Joe, I heard you shot your woman down
+      You shot her down, now
+      Hey Joe, I heard you shot your old lady down
+      You shot her down to the ground
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 4]</span>
+      Yes I did, I shot her
+      You know, I caught her messing around, messing around town
+      Yes I did, I shot her
+      You know, I caught my old lady messing around town
+      And I gave her the gun, I shot her!
+    </p>
+
+    <p>
+      <span class="indicators">[Guitar Solo/Bridge]</span>
+      Hey Joe, alright
+      Shoot her one more time, baby
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 5]</span>
+      Hey Joe, where you going to run to now?
+      Where you going to run to?
+      Hey Joe, I said, where you going to run to now?
+      Where you, where you going to go?
+      Well, dig it
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 6]</span>
+      I'm going way down south
+      Way down to Mexico way, alright
+      I'm going way down south
+      Way down where I can be free
+      There's no one going to find me
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 7]</span>
+      Ain't no hangman going to
+      He ain't gonna put a rope around me
+      You better believe it right now, I got to go now
+    </p>
+
+    <p>
+      <span class="indicators">[Outro]</span>
+      Hey Joe, you better run on down
+      Goodbye everybody, ow!
+      Hey Joe, run on down
+    </p>
+    `,
+    details: "",
+  },
+  {
+    songCover: "./img/ab67616d0000b273e77142abb2babcae62a06528.jpg",
+    title: "Piece of My Heart",
+    artist: "Janis Joplin",
+    song: "./audio/Piece of My Heart.mp3",
+    duration: "4.15",
+    lyrics: `
+    <p><span class="indicators">[Intro]</span></p>
+
+    <p>
+      Oh, come on, come on, come on, come on
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 1]</span>
+      Didn't I make you feel like you were the only man? Yeah
+      An' didn't I give you nearly everything that a woman possibly can?
+      Honey, you know I did
+    </p>
+
+    <p>
+      <span class="indicators">[Pre-Chorus]</span>
+      And, and each time I tell myself that I, well I think I've had enough
+      But I'm gonna, gonna show you baby, that a woman can be tough
+      I want you to come on, come on, come on, come on and take it
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      Take another little piece of my heart now, baby
+      Oh, oh, break it
+      Break another little bit of my heart now, darling, yeah, yeah, yeah, yeah
+      Oh, oh, have a
+      Have another little piece of my heart now, baby
+      Well you know you got it, if it makes you feel good
+      Oh, yes indeed
+    </p>
+
+    <p>
+      <span class="indicators">[Verse 2]</span>
+      You're out on the streets looking good
+      And baby deep down in your heart I guess you know that it ain't right
+      Never, never, never, never, never, never hear me when I cry at night
+      Babe and I cry all the time
+    </p>
+
+    <p>
+      <span class="indicators">[Pre-Chorus]</span>
+      But each time I tell myself that I, well I can't stand the pain
+      But when you hold me in your arms, I'll sing it once again
+      I'll say come on, come on, come on, come on and take it
+    </p>
+
+    <p>
+      <span class="indicators">[Chorus]</span>
+      Take another little piece of my heart now, baby
+      Oh, oh, break it
+      Break another little bit of my heart now, darling, yeah
+      Oh, oh, have a
+      Have another little piece of my heart now, baby
+      Well you know you got it, child, if it makes you feel good
+    </p>
+
+    <p>
+      I need you to come on, come on, come on, come on and take it
+      Take another little piece of my heart now, baby
+      Oh, oh, break it
+      Break another little bit of my heart, now darling, yeah, c'mon now
+      Oh, oh, have a
+      Have another little piece of my heart now, baby
+      You know you got it -
+    </p>
+
+    <p>
+      Waaaaah!
+    </p>
+
+    <p>
+      Take it, take another little piece of my heart now, baby
+      Oh, oh, break it
+      Break another little bit of my heart, now darling, yeah, yeah, yeah, yeah
+      Oh, oh, have a
+      Have another little piece of my heart now, baby, hey
+      You know you got it, child, if it makes you feel good
+    </p>
+
+    <p>
+      <span class="indicators">[Outro]</span>
+    </p>
+    `,
+    details: "",
+  },
+];
+
+let songPlay = new Audio();
+let songIndex = 0;
+
+const listOfSongs = document.querySelector("[data-list-of-songs]");
+const libraryBtn = document.querySelector("[data-library-btn]");
+const returnBtn = document.querySelector("[data-return-btn]");
+const playBtn = document.querySelector("[data-play-btn]");
+const leftArrow = document.querySelector("[data-left-arrow]");
+const rightArrow = document.querySelector("[data-right-arrow]");
+const songText = document.querySelector("[data-song-text]");
+const detailsBtn = document.querySelector("[data-details-btn]");
+const lyricsBtn = document.querySelector("[data-lyrics-btn]");
+const progressBar = document.querySelector("[data-progress-bar]");
+
+//Event listners
+
+libraryBtn.addEventListener("click", openListOfSongs);
+returnBtn.addEventListener("click", openListOfSongs);
+playBtn.addEventListener("click", playSong);
+leftArrow.addEventListener("click", previousSong);
+rightArrow.addEventListener("click", nextSong);
+detailsBtn.addEventListener("click", () => {
+  displayDetails(arrayOfSongs[songIndex].details);
+});
+lyricsBtn.addEventListener("click", () => {
+  displayLyrics(arrayOfSongs[songIndex].lyrics);
+});
+songPlay.addEventListener("loadeddata", updateTime);
+progressBar.addEventListener("click", (e) => {
+  playSongBySelectedTime(e);
+});
+songPlay.addEventListener("ended", songEnded);
+
+//Opens list of songs
+
+function openListOfSongs() {
+  listOfSongs.classList.toggle("unhide");
+}
+
+//Playing song and changing play buton src from pause to play
+
+let isActive = false;
+
+function playSong() {
+  if (!isActive) {
+    isActive = true;
+    playBtn.childNodes[1].src = "./img/pause.svg";
+    songPlay.play();
+  } else {
+    isActive = false;
+    playBtn.childNodes[1].src = "./img/play-button-arrowhead.svg";
+    songPlay.pause();
+  }
+}
+
+//Changes songs in the header (title, text, img, song src) by pressing arrow buttons
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > arrayOfSongs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(
+    arrayOfSongs[songIndex].songCover,
+    arrayOfSongs[songIndex].title,
+    arrayOfSongs[songIndex].lyrics,
+    arrayOfSongs[songIndex].song,
+    arrayOfSongs[songIndex].details
+  );
+
+  playBtn.childNodes[1].src = "./img/pause.svg";
+  isActive = true;
+  songPlay.play();
+}
+
+function previousSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = arrayOfSongs.length - 1;
+  }
+
+  loadSong(
+    arrayOfSongs[songIndex].songCover,
+    arrayOfSongs[songIndex].title,
+    arrayOfSongs[songIndex].lyrics,
+    arrayOfSongs[songIndex].song,
+    arrayOfSongs[songIndex].details
+  );
+
+  playBtn.childNodes[1].src = "./img/pause.svg";
+  isActive = true;
+  songPlay.play();
+}
+
+//Loads first song from arrayOfSongs and displays it
+
+function loadSong(songCover, title, lyrics, song) {
+  const songImg = document.querySelector("[data-song-img]");
+  const songTitle = document.querySelector("[data-song-title]");
+
+  songPlay.src = song;
+
+  songImg.src = songCover;
+  songTitle.textContent = title;
+  songText.innerHTML = lyrics;
+}
+
+//Displays details or lyrics
+
+function displayDetails(currentDetails) {
+  songText.innerHTML = currentDetails;
+}
+
+function displayLyrics(currentDetails) {
+  songText.innerHTML = currentDetails;
+}
+
+//Loops through and displays all songs from arrayOfSongs in the listOfSongs
+
+arrayOfSongs.forEach((song) => {
+  const songLibraryDiv = document.createElement("div");
+  const songLibraryColumn = document.createElement("div");
+  const songLibraryTitle = document.createElement("h2");
+  const songLibraryArtist = document.createElement("p");
+  const songLibraryImg = document.createElement("img");
+  const songLibraryTime = document.createElement("p");
+  const songLibraryPlay = document.createElement("button");
+  const songLibraryPlayInner = document.createElement("img");
+
+  songLibraryDiv.classList.add("song-library-div", "rounded");
+
+  songLibraryImg.src = song.songCover;
+  songLibraryImg.classList.add("song-library-img");
+
+  songLibraryColumn.classList.add("song-library-column");
+
+  songLibraryTitle.textContent = song.title;
+  songLibraryTitle.classList.add("medium-text");
+
+  songLibraryArtist.textContent = `artist: ${song.artist}`;
+  songLibraryArtist.classList.add("small-text");
+
+  songLibraryTime.textContent = `duration: ${song.duration}`;
+  songLibraryTime.classList.add("xsmall-text");
+
+  songLibraryPlay.setAttribute("data-library-play-btns", "");
+
+  songLibraryPlayInner.src = "./img/play-button-arrowhead.svg";
+
+  songLibraryPlay.append(songLibraryPlayInner);
+
+  songLibraryColumn.append(
+    songLibraryTitle,
+    songLibraryArtist,
+    songLibraryTime
+  );
+
+  songLibraryDiv.append(songLibraryImg, songLibraryColumn, songLibraryPlay);
+
+  listOfSongs.appendChild(songLibraryDiv);
+});
+
+//Loops through library songs and after clicking play plays song, closes list, and changes play icon in header
+
+const libraryPlayBtns = document.querySelectorAll("[data-library-play-btns]");
+
+libraryPlayBtns.forEach((libraryPlayBtn, index) => {
+  libraryPlayBtn.addEventListener("click", () => {
+    playLibrarySong(index);
+  });
+});
+
+function playLibrarySong(index) {
+  loadSong(
+    arrayOfSongs[index].songCover,
+    arrayOfSongs[index].title,
+    arrayOfSongs[index].lyrics,
+    arrayOfSongs[index].song,
+    arrayOfSongs[index].details
+  );
+
+  playBtn.childNodes[1].src = "./img/pause.svg";
+  isActive = true;
+  songPlay.play();
+
+  openListOfSongs();
+}
+
+//Updates time elements
+
+function updateTime() {
+  const fullTimeEl = document.querySelector("[data-full-time]");
+  const currentTimeEl = document.querySelector("[data-current-time]");
+
+  fullTimeEl.textContent = convertNumbers(songPlay.duration);
+
+  songPlay.addEventListener("timeupdate", () => {
+    currentTimeEl.textContent = convertNumbers(songPlay.currentTime);
+    updateProgressBar();
+  });
+}
+
+//Updating progess bar status
+
+function updateProgressBar() {
+  const fullTime = songPlay.duration;
+  const currentTime = songPlay.currentTime;
+  const progressSize = (current, overall, width) => (current / overall) * width;
+  const progressBarCtx = progressBar.getContext("2d");
+
+  progressBarCtx.fillStyle = "#fff";
+  progressBarCtx.fillRect(0, 0, progressBar.width, progressBar.height);
+
+  progressBarCtx.fillStyle = "#9f72ff";
+  progressBarCtx.fillRect(
+    0,
+    0,
+    progressSize(currentTime, fullTime, progressBar.width),
+    progressBar.height
+  );
+}
+
+function playSongBySelectedTime(timeClicked) {
+  const progressBarWidth = progressBar.getBoundingClientRect().width;
+  const amountComplited =
+    (timeClicked.clientX - progressBar.getBoundingClientRect().left) /
+    progressBarWidth;
+
+  songPlay.currentTime = (songPlay.duration || 0) * amountComplited;
+}
+
+//Coverting numbers to minutes and seconds for updatetime and updateprogressbar
+
+function convertNumbers(value) {
+  const minutes = Math.floor(value / 60);
+  const seconds = Math.floor(value % 60);
+
+  if (seconds < 10) {
+    return `${minutes}:0${seconds}`;
+  } else {
+    return `${minutes}:${seconds}`;
+  }
+}
+
+function songEnded() {
+  isActive = false;
+  playBtn.childNodes[1].src = "./img/replay_white_36dp.svg";
+}
+
+loadSong(
+  arrayOfSongs[0].songCover,
+  arrayOfSongs[0].title,
+  arrayOfSongs[0].lyrics,
+  arrayOfSongs[0].song,
+  arrayOfSongs[0].details
+);
